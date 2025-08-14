7013c0b6dc02507eca8b3764ad192d3d16ec841a
Meharban Ali <alimeharban760@gmail.com>
2025-08-14 11:06:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20.7 MB (23.96%)

--- my-folder/0136-single-number/solution.cpp
+++ my-folder/0136-single-number/solution.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int size = nums.size();
-        int ans = 0;
-        for(int i=0;i<size;i++){
-            ans  = ans^nums[i];
+        int n = nums.size();
+        int ans=  0;
+        for(int i=0;i<n;i++){
+            ans = ans^nums[i];
         }
         return ans;
     }
