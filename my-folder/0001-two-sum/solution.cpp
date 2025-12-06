ac5ecead068634ea9148a25805f941782eb693ff
Meharban Ali <alimeharban760@gmail.com>
2025-12-06 07:43:19 +0000

    [LeetCode Sync] Runtime - 100 ms (6.45%), Memory - 13.9 MB (99.80%)

--- my-folder/0001-two-sum/solution.cpp
+++ my-folder/0001-two-sum/solution.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int tar) {
+    vector<int> twoSum(vector<int>& arr, int tar) {
         vector<int> ans;
-        int n = nums.size();
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i] + nums[j] == tar){
+        int n = arr.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] + arr[j] == tar) {
                     ans.push_back(i);
                     ans.push_back(j);
-                    return ans;
+                    return ans;   // return the answer
                 }
-            } 
+            }
         }
-       return ans;
-    }	
+        return {}; // return empty if no pair found
+    }
 };
+
