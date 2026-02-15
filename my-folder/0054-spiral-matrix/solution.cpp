6de3fad0eadce0e73b0b76586fabb5c59da6ff14
Meharban Ali <alimeharban760@gmail.com>
2026-02-15 08:06:07 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.2 MB (98.43%)

---
+++ my-folder/0054-spiral-matrix/solution.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+        vector<int> ans;
+
+        int srow = 0, scol = 0;
+        int erow = m - 1, ecol = n - 1;
+
+        while (srow <= erow && scol <= ecol) {
+
+            // top
+            for (int j = scol; j <= ecol; j++) {
+                ans.push_back(mat[srow][j]);
+            }
+
+            // right
+            for (int i = srow + 1; i <= erow; i++) {
+                ans.push_back(mat[i][ecol]);
+            }
+
+            // bottom
+            if (srow < erow) {
+                for (int j = ecol - 1; j >= scol; j--) {
+                    ans.push_back(mat[erow][j]);
+                }
+            }
+
+            // left
+            if (scol < ecol) {
+                for (int i = erow - 1; i > srow; i--) {
+                    ans.push_back(mat[i][scol]);
+                }
+            }
+
+            // update boundaries
+            srow++;
+            erow--;
+            scol++;
+            ecol--;
+        }
+
+        return ans;
+    }
+};
+
