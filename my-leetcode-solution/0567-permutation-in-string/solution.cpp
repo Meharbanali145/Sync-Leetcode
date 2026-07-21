fc2f5615d915e09eb1a3c7ff7e3a566f7e09c3df
Meharban Ali <alimeharban760@gmail.com>
2026-07-21 01:23:41 +0000

    [LeetCode Sync] Runtime - 207 ms (17.28%), Memory - 9.7 MB (98.74%)

---
+++ my-leetcode-solution/0567-permutation-in-string/solution.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+    bool isFreqSame(int freq1[],int freq2[]){
+        for(int i=0;i<26;i++){
+            if(freq1[i] != freq2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool checkInclusion(string s1, string s2) {
+        int freq[26] = {0};
+        for(int i=0;i<s1.length();i++){
+            freq[s1[i]-'a']++;
+        }
+
+        int windSize = s1.length();
+        for(int i=0;i<s2.length();i++){
+            int windIndx = 0, indx = i;
+            int windFreq[26] = {0};
+            while(windIndx < windSize && indx < s2.length()){
+                windFreq[s2[indx]-'a']++;
+                windIndx++;indx++;
+            }
+            if(isFreqSame(freq,windFreq)){
+                return true;
+            }
+        }
+        return false;
+    }
+};
