db7754bde86324fb0f14badb9248b07bf0a7664e
Meharban Ali <alimeharban760@gmail.com>
2026-07-25 01:21:02 +0000

    [LeetCode Sync] Runtime - 3 ms (12.31%), Memory - 13.9 MB (14.69%)

---
+++ my-leetcode-solution/0443-string-compression/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int n = chars.size();
+        int indx = 0;
+        for(int i=0;i<n;i++){
+            int count = 0;
+            char ch = chars[i];
+            while(i<n && chars[i] == ch){
+                count++;i++;
+            }
+            if(count == 1){
+                chars[indx++] = ch;
+            }else{
+                chars[indx++] = ch;
+                string str = to_string(count);
+                for(char dig: str){
+                    chars[indx++] = dig;
+                }
+            }
+            i--;
+        }
+        chars.resize(indx);
+        return indx;
+    }
+};
