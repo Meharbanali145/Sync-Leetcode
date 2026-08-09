36018f1c4d76e4e17dc0ae800e0601f10d6a7665
Meharban Ali <alimeharban760@gmail.com>
2026-08-09 02:02:21 +0000

    [LeetCode Sync] Runtime - 464 ms (17.50%), Memory - 95.4 MB (62.88%)

---
+++ my-leetcode-solution/0204-count-primes/solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+    let isPrime = new Array(n).fill(true);
+    let count= 0;
+
+    for(let i=2;i<n;i++){
+        if(isPrime[i]){
+            count++;
+        }
+        for(let j=i*2;j<n;j=j+i){
+            isPrime[j]=false;
+        }
+    }
+    return count;
+};
